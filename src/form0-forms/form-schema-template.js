7cf8b9d421562d35b9e84eb531465e51eca9b257
Stefano Pompei <35841717+aragornii@users.noreply.github.com>
2025-08-10 21:43:41 +0200

    chore: update form schema template

--- src/form0-forms/form-schema-template.js
+++ src/form0-forms/form-schema-template.js
@@ -524,11 +524,11 @@ export const defaultFormTemplate = {
         visible: false,
         visible_conditions: {
           and: [
-            { field_id: 'ea3a1', operator: 'equal_to', value: 'yes' },
+            { field_id: 'can_vote', operator: 'equal_to', value: 'yes' },
             {
               or: [
-                { field_id: 'ccbb56', operator: 'greater_than', value: 20 },
-                { field_id: 'ef661', operator: 'equal_to', value: 'Bob' },
+                { field_id: 'age', operator: 'greater_than', value: 20 },
+                { field_id: 'first_name', operator: 'equal_to', value: 'Bob' },
               ],
             },
           ],
